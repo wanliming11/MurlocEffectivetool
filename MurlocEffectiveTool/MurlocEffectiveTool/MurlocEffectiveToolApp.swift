252fdf0ca44e33a9f0eb8eba0e497f95db1e8d4b
万力铭 <wanliming@douyu.tv>
2022-11-17 23:58:19 +0800

    init the public project

---
+++ MurlocEffectiveTool/MurlocEffectiveTool/MurlocEffectiveToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  MurlocEffectiveToolApp.swift
+//  MurlocEffectiveTool
+//
+//  Created by Murloc Wan on 2022/11/17.
+//
+
+import SwiftUI
+
+@main
+struct MurlocEffectiveToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
